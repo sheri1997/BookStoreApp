5cbef0706a0abc4f361fdb600f2da23f577a7b94
Shreesh Pandey <shvspandey@gmail.com>
2022-02-08 15:45:55 +0530

    Structure of the Project Pushed into Master Branch

---
+++ BookStoreRepositry/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookStoreRepositry
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
